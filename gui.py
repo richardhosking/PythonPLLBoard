b71de100925e9d9291b2a80f4e4851f4ef2ab7b3
richard <richardh@iinet.net.au>
2019-12-14 10:53:13 +0930

    second iteration of interface

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@
 # Frequency and amplitude data can be entered via
 # a touchscreen interface 
 
-from tkinter import * 
+from Tkinter import * 
 
 # Definitions and code for ADF4531  
 
@@ -14,11 +14,23 @@ mainwindow = Tk()
 mainwindow.title("RF Signal Generator")
 mainwindow.geometry('650x300')
 
+#Frames
+# Frequency control frame to enclose freq widgets
+freq = Frame(mainwindow,  borderwidth = 10)
+freq.grid(column = 0,   row = 0,  sticky=N)
+# Frame to enclose level widgets
+levels = Frame(mainwindow, borderwidth = 10)
+levels.grid(column = 1,  row = 0,  sticky=N)
+# Keys to enter data 
+# Inside a frame called keypad
+keypad = Frame(mainwindow,  borderwidth = 10)
+keypad.grid(column = 2,  row = 0,  sticky=(N, S))
+
 # Create instances of the ADF4531 and assign ports 
 Gen1 = aDFClass.ADF4531("Gen1")
 
 # Set ports acording to wiring of RPi
-# Broadcom witring convention
+# Broadcom wiring convention
 Gen1.chipEnable = 17 # Physical pin 11
 Gen1.loadEnable = 18  # Physical pin 12 
 Gen1.dataClock = 27  # Physical pin 13
@@ -52,7 +64,9 @@ Gen3.setup_port()
 
 # Handlers - need to be declared before widget is defined
 def clickedMute1():
-     # Cycle power setting between -4dBm, -1dBm, +2dBm, +5dBm and MUTE
+    Gen1message =  StringVar()
+
+     # Cycle power setting between -9dBm, -6dBm, -3dBm, 0dBm and MUTE
     if Gen1.mainPower == '11':
         Gen1.mainRFEnabled = '0'
         Gen1.mainPower ='00'
@@ -60,22 +74,23 @@ def clickedMute1():
     elif  Gen1.mainRFEnabled == '0':
         Gen1.mainRFEnabled ='1'
         Gen1.mainPower ='00'
-        Gen1message = '   -9   '
+        Gen1message = '-9dBm '
     elif  Gen1.mainPower =='00':
         Gen1.mainPower ='01'
-        Gen1message = '   -6   '
-    elif  Gen1.mainPower =='01':
+        Gen1message = '-6dBm '
+    elif  Gen1.mainPower == '01':
         Gen1.mainPower ='10'
-        Gen1message = '   -3   '
+        Gen1message = '-3dBm '
     elif  Gen1.mainPower =='10':
         Gen1.mainPower ='11'
-        Gen1message = '   0   '
-
+        Gen1message = '0dBm '
+        
+    levlbl1 = Label(levels, text=Gen1message)
+    levlbl1.grid(column=1, row=2,  padx=5)
     Gen1.update()
-    lbl = Label(mainwindow, text=Gen1message) 
-    lbl.grid(column=2, row=3)
 
 def clickedMute2():
+    Gen2message =  StringVar()
         # Cycle power setting between -4dBm, -1dBm, +2dBm, +5dBm and MUTE
     if Gen2.mainPower == '11':
         Gen2.mainRFEnabled = '0'
@@ -84,22 +99,23 @@ def clickedMute2():
     elif  Gen2.mainRFEnabled == '0':
         Gen2.mainRFEnabled ='1'
         Gen2.mainPower ='00'
-        Gen2message = '   -9   '
+        Gen2message = '-9dBm '
     elif  Gen2.mainPower =='00':
         Gen2.mainPower ='01'
-        Gen2message = '   -6   '
+        Gen2message = '-6dBm '
     elif  Gen2.mainPower =='01':
         Gen2.mainPower ='10'
-        Gen2message = '   -3   '
+        Gen2message = '-3dBm '
     elif  Gen2.mainPower =='10':
         Gen2.mainPower ='11'
-        Gen2message = '   0   '
+        Gen2message = ' 0dBm '
 
     Gen2.update()
-    lbl = Label(mainwindow, text=Gen2message) 
-    lbl.grid(column=4, row=3)
+    levlbl2 = Label(levels, text=Gen2message)
+    levlbl2.grid(column=1, row=5,  padx=5)
 
 def clickedMute3():
+    Gen3message =  StringVar()    
          # Cycle power setting between -4dBm, -1dBm, +2dBm, +5dBm and MUTE
          # In practice at output levels are -9, -6, -3 and 0dBm 
     if Gen3.mainPower == '11':
@@ -109,20 +125,20 @@ def clickedMute3():
     elif  Gen3.mainRFEnabled == '0':
         Gen3.mainRFEnabled ='1'
         Gen3.mainPower ='00'
-        Gen3message = '   -9   '
+        Gen3message = '-9dBm '
     elif  Gen3.mainPower =='00':
         Gen3.mainPower ='01'
-        Gen3message = '   -6   '
-    elif  Gen3.mainPower =='01':
+        Gen3message = '-6dBm '
+    elif  Gen3.mainPower =='01': 
         Gen3.mainPower ='10'
-        Gen3message = '   -3   '
+        Gen3message = '-3dBm '
     elif  Gen3.mainPower =='10':
         Gen3.mainPower ='11'
-        Gen3message = '   0   '
+        Gen3message = '0dBm '
 
     Gen3.update()
-    lbl = Label(mainwindow, text=Gen3message) 
-    lbl.grid(column=6, row=3)
+    levlbl3 = Label(levels, text=Gen3message)
+    levlbl3.grid(column=1, row=8,  padx=5)
 
 def clickedUpdate():
     # these varaiables need to be a StringVar to be used in widget
@@ -132,60 +148,47 @@ def clickedUpdate():
     # If there is data entered 
     if mainwindow.focus_get().get():
         freqEntered =int(mainwindow.focus_get().get())
+
+   
+        if freqEntered > 4400000:  # exceeds max VCO freq
+           # Bottom row message
+           error.set('Error!')
+           errorText.set('  Freq > max  ')
+         
+           # and clear data
+           mainwindow.focus_get().delete(0, END)
+    
+        elif freqEntered < 34275:  # Min freq of module 34.275 MHz 
+           error.set('Error!')
+           errorText.set('  Freq < min  ')
+          # Error/Message labels       
+          
+           mainwindow.focus_get().delete(0, END)
+                  
+        else:
+           error.set('          ')
+           errorText.set('Gen updated')
+           # Error/Message labels
+           
+           if mainwindow.focus_get() == freqGen1:
+              pass#Gen1.calculate_freq(freqEntered)
+              Gen1.update()
+           elif mainwindow.focus_get() == freqGen2:
+              pass#Gen2.calculate_freq(freqEntered)
+              Gen2.update()
+           elif mainwindow.focus_get() == freqGen3:
+              pass#Gen3.calculate_freq(freqEntered)
+              Gen3.update()
+       
     else:
         # if no data erase previous error message and return 
        error = '          '
        errorText = '          '
-       lbl1 = Label(mainwindow, textvariable=error)
-       lbl1.grid(column=0, row=10)
-       lbl2 = Label(mainwindow, textvariable=errorText)
-       lbl2.grid(column=2, row=10)
-       return
-    
-    if freqEntered > 4400000:  # exceeds max VCO freq
-       # Bottom row message
-       error.set('Error!')
-       errorText.set('  Freq > max  ')
-             # Error/Message labels
-       lbl1 = Label(mainwindow, textvariable=error)
-       lbl1.grid(column=0, row=10)
-       lbl2 = Label(mainwindow, textvariable=errorText)
-       lbl2.grid(column=2, row=10)
-       
-       # and clear data
-       mainwindow.focus_get().delete(0, END)
-       return
-       
-    elif freqEntered < 34275:  # Min freq of module 34.275 MHz 
-       error.set('Error!')
-       errorText.set('  Freq < min  ')
-      # Error/Message labels       
-       lbl1 = Label(mainwindow, textvariable=error)
-       lbl1.grid(column=0, row=10)
-       lbl2 = Label(mainwindow, textvariable=errorText)
-       lbl2.grid(column=2, row=10)
-       
-       mainwindow.focus_get().delete(0, END)
-       return
-       
-    else:
-       error.set('          ')
-       errorText.set('Gen updated')
-       # Error/Message labels
-       lbl1 = Label(mainwindow, textvariable=error)
-       lbl1.grid(column=0, row=10)
-       lbl2 = Label(mainwindow, textvariable=errorText)
-       lbl2.grid(column=2, row=10)
-       
-    if mainwindow.focus_get() == freqGen1:
-       Gen1.calculate_freq(freqEntered)
-       Gen1.update()
-    elif mainwindow.focus_get() == freqGen2:
-       Gen2.calculate_freq(freqEntered)
-       Gen2.update()
-    elif mainwindow.focus_get() == freqGen3:
-       Gen3.calculate_freq(freqEntered)
-       Gen3.update()
+
+    lbl1 = Label(keypad, textvariable=error)
+    lbl1.grid(column=0, row=5)
+    lbl2 = Label(keypad, textvariable=errorText)
+    lbl2.grid(column=1, row=5,  columnspan=2)
     
 #Insert digit at end of variable in window in focus   
 def keypad1():
@@ -217,92 +220,87 @@ def keypadback():
     index = len(data)-1
     mainwindow.focus_get().delete(index) 
 
-#Widgets 
-# First row labels
-lbl = Label(mainwindow, text="Generator 1")
-lbl.grid(column=2, row=0)
-lbl = Label(mainwindow, text="Generator 2")
-lbl.grid(column=4, row=0)
-lbl = Label(mainwindow, text="Generator 3")
-lbl.grid(column=6, row=0)
-
-# Second row 
-# Frequency indicators
-lbl = Label(mainwindow, text="Frequency")
+#FREQ FRAME
+#Widgets frequency frame
+lbl = Label(freq, text="Frequency")
 lbl.grid(column=0, row=1)
 
+lbl = Label(freq, text="Generator 1")
+lbl.grid(column=0,   row=2)
+
 freqGen1text= StringVar()
-freqGen1 = Entry(mainwindow,width=10,  textvariable=freqGen1text)
-freqGen1.grid(column=2, row=1)
+freqGen1 = Entry(freq,width=10,  textvariable=freqGen1text)
+freqGen1.grid(column=0,  row=3,  pady=3)
 
-lbl = Label(mainwindow, text="KHz")
-lbl.grid(column=3, row=1)
+lbl = Label(freq, text="KHz")
+lbl.grid(column=1,  row=3)
 
-freqGen2 = Entry(mainwindow,width=10)
-freqGen2.grid(column=4, row=1)
+# Spacer
+lbl = Label(freq)
+lbl.grid(column=0, row=4)
 
-lbl = Label(mainwindow, text="KHz")
-lbl.grid(column=5, row=1)
+lbl = Label(freq, text="Generator 2")
+lbl.grid(column=0,   row=5)
 
-freqGen3 = Entry(mainwindow,width=10)
-freqGen3.grid(column=6, row=1)
+freqGen2text= StringVar()
+freqGen2 = Entry(freq,width=10,  textvariable=freqGen2text)
+freqGen2.grid(column=0, row=6,  pady=3)
 
-lbl = Label(mainwindow, text="KHz")
-lbl.grid(column=7, row=1)
+lbl = Label(freq, text="KHz")
+lbl.grid(column=1, row=6)
 
-# Third Row space 
+# Spacer
+lbl = Label(freq)
+lbl.grid(column=0, row=7)
 
-#Fourth Row - levels 
-lbl = Label(mainwindow, text="Level")
+lbl = Label(freq, text="Generator 3")
+lbl.grid(column=0,  row=8)
+
+freqGen3text= StringVar()
+freqGen3 = Entry(freq,width=10,  textvariable=freqGen3text)
+freqGen3.grid(column=0, row=9,  pady=3)
+
+lbl = Label(freq, text="KHz")
+lbl.grid(column=1, row=9)
+
+#LEVEL FRAME
+# Level widgets
+lbl = Label(levels, text="Level")
+lbl.grid(column=0, row=0)
+
+# Spacer
+lbl = Label(levels)
+lbl.grid(column=0, row=1)
+
+mute1 = Button(levels, text="Power/Mute", command=clickedMute1)
+mute1.grid(column=0, row=2)
+levlbl1 = Label(levels, text='MUTED')
+levlbl1.grid(column=1, row=2,  padx=5)
+
+# Spacer
+lbl = Label(levels)
 lbl.grid(column=0, row=3)
+lbl = Label(levels)
+lbl.grid(column=0, row=4)
 
-lbl = Label(mainwindow, text="MUTED")
-lbl.grid(column=2, row=3)
+mute2 = Button(levels, text="Power/Mute", command=clickedMute2)
+mute2.grid(column=0, row=5)
+levlbl2 = Label(levels, text='MUTED')
+levlbl2.grid(column=1, row=2,  padx=5)
 
-lbl = Label(mainwindow, text="dBm")
-lbl.grid(column=3, row=3)
-
-lbl = Label(mainwindow, text="MUTED")
-lbl.grid(column=4, row=3)
-
-lbl = Label(mainwindow, text="dBm")
-lbl.grid(column=5, row=3)
-
-lbl = Label(mainwindow, text="MUTED")
-lbl.grid(column=6, row=3)
-
-lbl = Label(mainwindow, text="dBm")
-lbl.grid(column=7, row=3) 
-
-# Fifth row space
-
-# Sixth Row Mute 
-lbl = Label(mainwindow, text="")
+# Spacer
+lbl = Label(levels)
 lbl.grid(column=0, row=6)
+lbl = Label(levels)
+lbl.grid(column=0, row=7)
 
-mute1 = Button(mainwindow, text="Power/Mute", command=clickedMute1)
-mute1.grid(column=2, row=6)
+mute3 = Button(levels, text="Power/Mute", command=clickedMute3)
+mute3.grid(column=0, row=8)
+levlbl3 = Label(levels, text='MUTED')
+levlbl3.grid(column=1, row=2,  padx=5)
 
-lbl = Label(mainwindow, text="")
-lbl.grid(column=3, row=5)
-
-mute2 = Button(mainwindow, text="Power/Mute", command=clickedMute2)
-mute2.grid(column=4, row=6)
-
-lbl = Label(mainwindow, text="")
-lbl.grid(column=5,  row=5)
-
-mute3 = Button(mainwindow, text="Power/Mute", command=clickedMute3)
-mute3.grid(column=6, row=6)
-
-lbl = Label(mainwindow, text="")
-lbl.grid(column=7, row=5)
-
-# Keys to enter data 
-# Inside a frame called keypad
-keypad = Frame(mainwindow,  bg  = "yellow",  borderwidth = 3)
-keypad.grid(column = 12,  row = 2)
-
+#KEYPAD
+# Keypad widgets
 btn = Button(keypad, text=" 1 ",   command=keypad1)
 btn.grid(column=0, row=0)
 
@@ -340,8 +338,8 @@ btn = Button(keypad, text=" < ",   command=keypadback)
 btn.grid(column=2, row=3)
 
 # Update generator which has current focus 
-btn = Button(mainwindow, text="Update Generator", command=clickedUpdate)
-btn.grid(column=12, row=6)
+btn = Button(keypad, text="Update Generator", command=clickedUpdate)
+btn.grid(column=0, row=4,  columnspan=3)
 
 
 
